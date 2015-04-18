e62899212faa2fb35505380f45a0797eed370098
SecComm <seccommclient@gmail.com>
2015-04-18 01:47:57 +0100

    Adding build-steps for script-link

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,63 @@
-grunt.initConfig({
-  jekyll: {                             // Task
-    options: {                          // Universal options
-      bundleExec: true,
-      src : '<%= app %>'
-    },
-    dist: {                             // Target
-      options: {                        // Target options
-        dest: '<%= dist %>',
-        config: '_config.yml,_config.build.yml'
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', '_scripts/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
       }
     },
-    serve: {                            // Another target
+    concat: {
       options: {
-        dest: '.jekyll',
-        drafts: true
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['_scripts/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'public/scripts/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/scripts/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jekyll: {                             // Task
+      options: {                          // Universal options
+        bundleExec: true,
+        src : '<%= app %>'
+      },
+      dist: {                             // Target
+        options: {                        // Target options
+          dest: '<%= dist %>',
+          config: '_config.yml,_config.build.yml'
+        }
+      },
+      serve: {                            // Another target
+        options: {
+          dest: '.jekyll',
+          drafts: true
+        }
       }
     }
-  }
-});
+  });
 
-grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jekyll');
 
-grunt.registerTask('default', ['jshint', 'jekyll']);
+  grunt.registerTask('default', ['jshint', 'jekyll']);
+};
--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 - [Jekyll](https://github.com/jekyll/jekyll) - Thanks to its creators, contributors and maintainers.
 - [Jekyll Now](https://github.com/barryclark/jekyll-now/) - Thanks to all involved in this great boilerplate.
-- [Hyde Theme](https://github.com/poole/hyde) - Looks great @poole!
+- [Hyde Theme](https://github.com/poole/hyde) - Looks great poole!
